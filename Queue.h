d539c1bc0a8c862529baa68e96b13f48425a81ad
Aram-Vn <aramvardanyan13@gmail.com>
2024-01-16 19:41:06 +0400

    format

--- Queue.h
+++ Queue.h
@@ -6,32 +6,33 @@ namespace my {
 	class Queue
 	{
 	public:
-		Queue(); // defolt constructor
+		Queue();  // defolt constructor
 		~Queue(); // destructor
 		
-		Queue(const Queue& other); // copy constructor 
-		Queue(Queue&& other) noexcept; //move constructor		
+		Queue(const Queue& other);     // copy constructor 
+		Queue(Queue&& other) noexcept; // move constructor		
 
 	public:
-		void enqueue(T val); //Inserts an element at the end of the queue i.e. at the rear end.
-		T dequeue(); // This operation removes and returns an element that is at the front end of the queue.		
-		T front(); // This operation returns the element at the front end without removing it.
-		T rear() const; //This operation returns the element at the rear end without removing it.
-		bool isEmpty() const; //This operation indicates whether the queue is empty or not.
-		size_t size() const; //This operation returns the size of the queue i.e. the total number of elements it contains.	
-
-	void print(); // prints elements
+		void enqueue(T val);  // Inserts an element at the end of the queue i.e. at the rear end.
+		T dequeue(); 		  // This operation removes and returns an element that is at the front end of the queue.		
+		T front(); 			  // This operation returns the element at the front end without removing it.
+		T rear() const; 	  // This operation returns the element at the rear end without removing it.
+		bool isEmpty() const; // This operation indicates whether the queue is empty or not.
+		size_t size() const;  // This operation returns the size of the queue i.e. the total number of elements it contains.	
+		void print();         // prints elements
 
 	private:
 		void realloc(); // reallocates memory by doubling it
 
 	private:
-		size_t m_cap;
-		size_t m_end; //m_size
-		size_t m_start; //m_i
-		T* m_ptr;
+		size_t m_cap;	// queu capasity
+		size_t m_end;   // current size
+		size_t m_start; // starting index
+		T* m_ptr;		// our queu
 	};
-	
-	#include "Queue_impl.hpp"
-}
+
+} // namespace my
+
+#include "Queue_impl.hpp"
+
 #endif //QUEUE_H
--- Queue_impl.hpp
+++ Queue_impl.hpp
@@ -2,133 +2,102 @@
 #define QUEUE_IMPL_HPP
 
 template <class T>
-my::Queue<T>::Queue() :
-	m_cap{0}, 
-	m_end{0},
-	m_start{0}, 
-	m_ptr{nullptr}
-{}
+my::Queue<T>::Queue() : m_cap{0}, m_end{0}, m_start{0}, m_ptr{nullptr} {}
 
-template <class T> 
-my::Queue<T>::~Queue()
-{
-	m_cap = 0;
-	m_end = 0;
-	m_start = 0;
-	if(m_ptr != nullptr){
-		delete[] m_ptr;
-	}
-}
-
-template <class T> 
-my::Queue<T>::Queue(const Queue& other) :
-	m_end{other.m_end},
-	m_cap{other.m_cap},
-	m_start{other.m_start},
-	m_ptr{new T[m_cap]} 
-{
-	for(size_t i = m_start; i < m_end; ++i){
-		this->m_ptr[i] = other.m_ptr[i]; 
-	}
-}
-
-template <class T>  
-my::Queue<T>::Queue(Queue&& other) noexcept :
-	m_end{other.m_end},
-	m_cap{other.m_cap}, 
-	m_start{other.m_start},
-	m_ptr{other.m_ptr} 
-{
-	other.m_end = 0;
-	other.m_cap = 0;
-	other.m_start = 0;
-	other.m_ptr = nullptr;
-}
-
-template <class T>  
-void my::Queue<T>::enqueue(T val)
-{
-	if(m_end == m_cap){
-		realloc();
-	}
-
-	m_ptr[m_end++] = val;
-}
-
-template <class T> 
-T my::Queue<T>::dequeue()
-{
-	T val = m_ptr[m_start];
-
-	++m_start;
-
-	if(m_start == m_cap / 2 && m_cap > 8){
-		size_t k = 0;
-
-		for(size_t i = m_start; i < m_end; ++i){
-			m_ptr[k] = m_ptr[i];
-			++k;
-		}
-	}	
-
-	return val;
-}
-
-
-template <class T>
-T my::Queue<T>::front()
-{
-	if(!isEmpty()){
-		return m_ptr[m_start];
-	} else {
-		std::cout << "for my::Queue<T>::front()\nQueue is Empty" << std::endl;
-		exit (0);
-	}
-}
-
-template <class T> 
-T my::Queue<T>::rear() const
-{
-	if(!isEmpty()){
-		return m_ptr[m_end - 1];
-	} else {
-		std::cout << "for my::Queue<T>::rear()\nQueue is Empty" << std::endl;
-		exit (0);
-	}
+template <class T> my::Queue<T>::~Queue() {
+  m_cap = 0;
+  m_end = 0;
+  m_start = 0;
+  if (m_ptr != nullptr) {
+    delete[] m_ptr;
+  }
 }
 
 template <class T>
-bool my::Queue<T>::isEmpty() const
-{
-	return this->size() == 0;
-}
-
-template <class T> 
-size_t my::Queue<T>::size() const
-{
-	return m_end - m_start;
+my::Queue<T>::Queue(const Queue &other)
+    : m_end{other.m_end}, m_cap{other.m_cap}, m_start{other.m_start},
+      m_ptr{new T[m_cap]} {
+  for (size_t i = m_start; i < m_end; ++i) {
+    this->m_ptr[i] = other.m_ptr[i];
+  }
 }
 
 template <class T>
-void my::Queue<T>::realloc()
-{
-	m_cap = (m_cap) ? 2 * m_cap : 1;
-
-	T* tmp_ptr = new T[m_cap];
-
-	for(size_t i = m_start; i < m_end; ++i){
-		tmp_ptr[i] = m_ptr[i]; 
-	}
-
-	delete[] m_ptr;
-	m_ptr = tmp_ptr;
+my::Queue<T>::Queue(Queue &&other) noexcept
+    : m_end{other.m_end}, m_cap{other.m_cap}, m_start{other.m_start},
+      m_ptr{other.m_ptr} {
+  other.m_end = 0;
+  other.m_cap = 0;
+  other.m_start = 0;
+  other.m_ptr = nullptr;
 }
 
-template <class T>
-void my::Queue<T>::print(){
-	for(size_t i = m_start; i < m_end; ++i){
-		std::cout << m_ptr[i] << std::endl;
-	}
+template <class T> void my::Queue<T>::enqueue(T val) {
+  if (m_end == m_cap) {
+    realloc();
+  }
+
+  m_ptr[m_end++] = val;
 }
 
-#endif //QUEUE_IMPL_HPP
+template <class T> T my::Queue<T>::dequeue() {
+  T val = m_ptr[m_start];
+
+  ++m_start;
+
+  if (m_start == m_cap / 2 && m_cap > 8) {
+    size_t k = 0;
+
+    for (size_t i = m_start; i < m_end; ++i) {
+      m_ptr[k] = m_ptr[i];
+      ++k;
+    }
+  }
+
+  return val;
+}
+
+template <class T> T my::Queue<T>::front() {
+  if (!isEmpty()) {
+    return m_ptr[m_start];
+  } else {
+    std::cout << "for my::Queue<T>::front()\nQueue is Empty" << std::endl;
+    exit(0);
+  }
+}
+
+template <class T> T my::Queue<T>::rear() const {
+  if (!isEmpty()) {
+    return m_ptr[m_end - 1];
+  } else {
+    std::cout << "for my::Queue<T>::rear()\nQueue is Empty" << std::endl;
+    exit(0);
+  }
+}
+
+template <class T> bool my::Queue<T>::isEmpty() const {
+  return this->size() == 0;
+}
+
+template <class T> size_t my::Queue<T>::size() const { return m_end - m_start; }
+
+template <class T> void my::Queue<T>::realloc() {
+  m_cap = (m_cap) ? 2 * m_cap : 1;
+
+  T *tmp_ptr = new T[m_cap];
+
+  for (size_t i = m_start; i < m_end; ++i) {
+    tmp_ptr[i] = m_ptr[i];
+  }
+
+  delete[] m_ptr;
+  m_ptr = tmp_ptr;
+}
+
+template <class T> void my::Queue<T>::print() {
+  for (size_t i = m_start; i < m_end; ++i) {
+    std::cout << m_ptr[i] << std::endl;
+  }
+}
+
+#endif // QUEUE_IMPL_HPP
